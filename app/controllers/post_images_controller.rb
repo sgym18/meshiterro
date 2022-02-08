d09affa536788164b5d8c93adc136613f6521ef2
su-kino <97873142+su-kino@users.noreply.github.com>
2022-02-08 01:21:50 +0000

    [Finish]4-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,7 +2,7 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -15,8 +15,9 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
-  
+
   private
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
