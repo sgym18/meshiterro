09621e40a5a031f17e36b7a031416960edab5d9d
su-kino <97873142+su-kino@users.noreply.github.com>
2022-02-09 07:24:21 +0000

    [Finish]4-20

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,8 @@ class FavoritesController < ApplicationController
     favorite.save
     redirect_to post_image_path(post_image)
   end
-  
+
+
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
